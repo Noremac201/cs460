9d90cdf3ddb39695ce5bde9d29b21f0e9fd2933b
Cameron Moberg <cjmoberg@gmail.com>
2019-03-17 08:13:16 -0700

    update rayclip

--- assign3/rayclip.cpp
+++ assign3/rayclip.cpp
@@ -14,6 +14,11 @@ using std::endl;
 const int WINDOW_WIDTH = 1440 / 2;
 const int WINDOW_HEIGHT = 900 / 2;
 
+struct Line {
+    Point2 a;
+    Point2 b;
+};
+
 void drawLine2(Point2 v, Point2 u)
 {
     glBegin(GL_LINES);
@@ -35,20 +40,37 @@ void drawBoundary(std::vector<Point2> pts) {
     glFlush();
 }
 
-double Clip_line(Point2 &a, Point2 &b, Point2 &c, Point2 &d)
+double Clip_line(const Point2& a, const Point2& b, const Point2 &c, const Point2 &d)
 {
-    if (inner(perp(d-c), b-a) != 0)
+    if (inner(perp(d-c), b-a) >= 0 + EPSILON ||
+            inner(perp(d-c), b-a) <= 0 - EPSILON)
     {
         double t = inner(perp(d-c), c-a)/inner(perp(d-c), b-a);
         double u = inner(perp(b-a), c-a)/inner(perp(d-c), b-a);
         // Ignore if t doesn't reach u, since we don't care
-        if (t > 0.0001 && u >= 0.0001 && u <= 1)
+        if (t > 0.0001 && u >= 0.0000001 && u <= 1.0005)
         {
-            b = a + (inner(perp(d-c), c-a)/inner(perp(d-c), b-a)) * (b - a);
             return t;
         }
     }
-    return -1;
+    return -100000;
+}
+
+std::vector<Line> getLines(std::vector<std::vector<Point2>> polys)
+{
+    std::vector<Line> lines;
+    for (std::vector<Point2> poly : polys)
+    {
+        for (int i = 0; i < poly.size(); i++)
+        {
+            if (i + 1 == poly.size())
+                lines.push_back(Line{poly[i], poly[0]});
+            else
+                lines.push_back(Line{poly[i], poly[i+1]});
+        }
+    }
+    return lines;
+
 }
 
 void part1() {
@@ -60,33 +82,54 @@ void part1() {
     p.push_back(Point2(400, 400));
     drawBoundary(p);
 
+    std::vector<Point2> pIn;
+    pIn.push_back(Point2(150, 140));
+    pIn.push_back(Point2(200, 150));
+    pIn.push_back(Point2(200, 200));
+    pIn.push_back(Point2(150, 200));
+    drawBoundary(pIn);
+
+    std::vector<std::vector<Point2>> aa;
+    aa.push_back(p);
+    aa.push_back(pIn);
+    std::vector<Line> lines = getLines(aa);
+
     Point2 a, b;
     // arbitrary points
     a = { 250, 250};
-    b = { 40, 120};
+    b = { 40, 135};
 
     for (int j = 0; j < 1000; j++)
     {
+        cout << "Iteration" << endl;
         double minT = 10000;
         Point2 u, v;
-        for (int i = 0;	i < p.size(); i++)
-        {
-            // Edits a and b in method 
-            double t = Clip_line(a, b, p[i], p[(1 + i) % p.size()]);
 
-            if (t == -1)
+
+        Point2 newB;
+        for (Line line : lines)
+        {
+            Point2 p1 = line.a;
+            Point2 p2 = line.b;
+            // Edits a and b in method 
+            double t = Clip_line(a, b, p1, p2);
+
+            if (t == -100000)
                 continue;
 
             if ( t < minT)
             {
                 minT = t;
-                u = p[i];
-                v = p[(1+i) % p.size()];
+                u = p1;
+                v = p2;
+                newB  = a + (inner(perp(p2-p1), p1-a)/inner(perp(p2-p1), b-a)) * (b - a);
             }
         }
-
+        
+        b = newB;
+        cout << "Selected: " << u.printable() << v.printable() << endl;
         Vector2 rray = b - a;
-        Vector2 re = reflect(rray, v-u);
+        Vector2 re = reflect(rray, u-v);
         Point2 zed = Point2(b.x + re.delta_x(), b.y + re.delta_y());
         drawLine2(a, b);
 
